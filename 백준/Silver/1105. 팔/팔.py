7df73944ea3a20cadd84b21f037484b4b5ac8e6d
Leecarrot <121033188+Leecarrot@users.noreply.github.com>
2025-01-30 20:07:42 +0900

    [Silver I] Title: 팔, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.py
@@ -0,0 +1,14 @@
+L, R = map(str, input().split())
+
+if len(L) != len(R):
+  print(0)
+else:
+  cnt = 0
+  for i in range(len(L)):
+    if L[i] == R[i] and L[i] !='8':
+      continue
+    if L[i] == '8' and R[i] =='8':
+      cnt+=1
+    else:
+      break
+  print(cnt)
