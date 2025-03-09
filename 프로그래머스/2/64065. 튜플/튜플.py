a8e349af33906207ca449e6a9a9f52f905862c3d
Leecarrot <121033188+Leecarrot@users.noreply.github.com>
2025-03-09 15:22:00 +0900

    [level 2] Title: 튜플, Time: 465.44 ms, Memory: 17.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,18 @@
+def solution(s):
+    result = []
+    
+    s = s[2:-2]
+    str_groups = s.split("},{")
+    
+    s1 = [list(map(int, group.split(','))) for group in str_groups]
+    
+    s2 = sorted(s1, key = lambda x:len(x))
+    
+    for i in s2:
+        if len(i) == 1:
+            result.append(i[0])
+        else:
+            for j in i:
+                if j not in result:
+                    result.append(j)
+    return result
