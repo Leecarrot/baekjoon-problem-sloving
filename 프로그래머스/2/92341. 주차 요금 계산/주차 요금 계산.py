7cf6c6437ca300cf647c65344feff8a6a9cf1b9c
Leecarrot <121033188+Leecarrot@users.noreply.github.com>
2025-03-18 20:08:52 +0900

    [level 2] Title: 주차 요금 계산, Time: 4.41 ms, Memory: 9.76 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,32 @@
+import math
+
+def solution(fees, records):
+    park = {}
+    result = []
+    for record in records:
+        time, num, a = record.split()
+        if num not in park:
+            park[num] = [time]
+        else:
+            park[num].append(time)
+            
+    for i in park:
+        if len(park[i]) % 2 != 0:
+            park[i].append('23:59')
+    
+    park = dict(sorted(park.items(), key = lambda x:x[0]))
+    
+    for i in park:
+        plus = 0
+        for j in range(len(park[i]) - 1, 0, -2):
+            a, b = park[i][j].split(':')
+            c, d = park[i][j - 1].split(':')
+            s = (int(a) * 60 + int(b)) - (int(c) * 60 + int(d)) 
+            plus += s
+        if plus > fees[0]:
+            final = fees[1] + math.ceil(((plus - fees[0])/fees[2])) * fees[3]
+            result.append(final)
+        else:
+            result.append(fees[1])
+    return result
+    
