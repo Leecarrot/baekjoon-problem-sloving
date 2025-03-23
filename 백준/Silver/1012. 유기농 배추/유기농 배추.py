772b3f3b3c3833f89469ffde34b948160b09d1ec
Leecarrot <121033188+Leecarrot@users.noreply.github.com>
2025-03-23 23:21:46 +0900

    [Silver II] Title: 유기농 배추, Time: 224 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,32 +1,40 @@
-import sys
+from collections import deque
 
-sys.setrecursionlimit(10000)
-
-def dfs(x, y):
-    if x < 0 or y < 0 or x >= m or y >= n:
-        return False
-    if graph[y][x] == 1:
-        graph[y][x] = 0
-        dfs(x, y-1)
-        dfs(x, y+1)
-        dfs(x-1, y)
-        dfs(x+1, y)
-        return True
-    return False
-
-t = int(input())
-
-for _ in range(t): 
+for _ in range(int(input())):
     m, n, k = map(int, input().split())
-    graph = [[0] * m for i in range(n)]
-
-    for i in range(k):
-        x, y = map(int, input().split())
-        graph[y][x] = 1
+    
+    graph = [[0 for _ in range(m)] for _ in range(n)]
+    
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[b][a] = 1
+        
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    def bfs(x, y):
+        que = deque([(x, y)])
+        
+        while que:
+            x, y = que.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if 0 <= nx < n and 0 <= ny < m:
+                    if graph[nx][ny] == 1 and not visited[nx][ny]:
+                        visited[nx][ny] = True
+                        que.append((nx, ny))
+        return 1
     
     result = 0
-    for i in range(m):
-        for j in range(n):
-            if dfs(i, j) == True:
-                result += 1
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1 and not visited[i][j]:
+                visited[i][j] = True
+                result += bfs(i, j)
+    
     print(result)
