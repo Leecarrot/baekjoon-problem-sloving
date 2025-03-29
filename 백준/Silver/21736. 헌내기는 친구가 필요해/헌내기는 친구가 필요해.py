be650792c7f40b7cea2ff15ba2352e8a24a78853
Leecarrot <121033188+Leecarrot@users.noreply.github.com>
2025-03-29 23:14:26 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 448 ms, Memory: 41684 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,32 +1,17 @@
 from collections import deque
-import sys
-sys.setrecursionlimit(100000)
 
 n, m = map(int, input().split())
-
 graph = []
-
+visited = [[False for _ in range(m)] for _ in range(n)]
 for _ in range(n):
-    graph.append(list(map(str, input())))
-    
-visited = [[False for i in range(m)] for i in range(n)]
+    graph.append(list(input()))
 
-ix = 0
-iy = 0
-
-dx = [1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 'I':
-            ix = i
-            iy = j
-            
 def bfs(x, y):
-    que = deque()
-    que.append((x, y))
-    result = 0
+    que = deque([(x, y)])
+    friend = 0
     
     while que:
         x, y = que.popleft()
@@ -36,14 +21,20 @@ def bfs(x, y):
             ny = y + dy[i]
             
             if 0 <= nx < n and 0 <= ny < m:
-                if (graph[nx][ny] == 'O' or graph[nx][ny] == 'I' or graph[nx][ny] == 'P') and not visited[nx][ny]:
+                if (graph[nx][ny] == 'O' or graph[nx][ny] == 'P') and not visited[nx][ny]:
                     visited[nx][ny] = True
                     if graph[nx][ny] == 'P':
-                        result += 1
+                        friend += 1
                     que.append((nx, ny))
-    if result == 0:
-        print('TT')
-    else:
-        print(result)
-                
-bfs(ix, iy)
+                    
+    return friend
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 'I':
+            visited[i][j] = True
+            result = bfs(i, j)
+            if result != 0:
+                print(result)
+            else:
+                print('TT')
