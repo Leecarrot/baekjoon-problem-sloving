dc98a07264c19b908eb59b00e92797426a59426f
Leecarrot <121033188+Leecarrot@users.noreply.github.com>
2025-11-22 22:22:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 268.54 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -2,15 +2,14 @@ def solution(ingredient):
     stack = []
     answer = 0
     
-    for x in ingredient:
-        stack.append(x)
+    for i in ingredient:
+        stack.append(i)
         
         if len(stack) >= 4:
             if stack[-4:] == [1, 2, 3, 1]:
-                # 4개 제거
                 for _ in range(4):
                     stack.pop()
                 answer += 1
-                
+    
     return answer
         
